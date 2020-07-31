bb4396ed7c27c5c908f4d9faa3b74131f068b2e4
Aaron Brady <aleebrady@gmail.com>
2020-07-31 19:48:12 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,50 @@
+def sort_array_asc(nums)
+  nums.sort
+end
 
+def sort_array_desc(nums)
+  nums.sort.reverse 
+end
+
+def sort_array_char_count(pets)
+  pets.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(swap)
+  swap[1], swap[2] = swap[2], swap[1]
+  swap
+end
+
+def reverse_array(nums)
+  nums.reverse 
+end
+
+def kesha_maker(sign)
+  new_sign = []
+  sign.each do |x|
+    x[2] = "$"
+    new_sign << x
+  end
+end
+
+def find_a(array)
+  array.select do |item|
+    item.start_with?("a")
+  end 
+end
+
+def sum_array(sum)
+  sum.inject {|x, y| x + y}
+end
+
+def add_s(words)
+  words.collect do |word|
+    if words[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
