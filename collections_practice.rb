d735e806b6f4109f59da088f83b154f51ddc7c9f
Daniel Gould <dmgould310@gmail.com>
2020-07-31 19:20:06 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,64 @@
+def sort_array_asc(array)
+  array.sort 
+end
+
+def sort_array_desc(array)
+  array.sort.reverse  
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length 
+  end 
+end
+
+def swap_elements(array)
+  array.sort do |a, b|
+    a[1] <=> b[2]
+  end 
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)
+array.each do |letter|
+  letter[2] = "$"
+  end 
+end 
+
+def find_a(array)
+array.select do |x|
+  x.start_with?("a")
+  end 
+end 
+
+def sum_array(array)
+  array.inject { |sum, n| sum + n }  
+end
+
+def add_s(array)
+  array.each_with_index.collect do |index, string|
+    if index == 1 
+    return string
+  else 
+    string << ("s")
+    end 
+  end 
+end 
+  
+  
+  def add_s(array)
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end
+end
+
+
+
 
